595cad6e193a1da8828b6defb07b34db80d96e99
jackson8en <jackson.8en@gmail.com>
2017-03-19 00:58:02 +0000

    :fireworks:  Initial Project Setup with Tile & Landscape Classes
    :fireworks:  Initial Project Setup with Tile & Landscape Classes have
    been added, no code yet.

---
+++ Mars Game/Assets/Model/Tile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+
+	public enum TileType {Loose, Rocks, Quicksand, Dirt, AncientGlacier};
+
+}
